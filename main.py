6f8764098ad7ad5fa5d6d33be87e12770195f894
rafetorcunmadran <rafetorcunmadran@gmail.com>
2024-12-12 12:47:58 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Flask, send_file, render_template, request
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return send_file('src/index.html')
+
+@app.route("/arama")
+def arama():
+    anahtarKelime = request.args.get('anahtarKelime','')
+    return render_template('arama.html', data=anahtarKelime)
+
+@app.route("/katalog")
+def katalog():
+    
+    tree = ET.parse('katalog.xml')
+    root = tree.getroot()
+
+    catalog = []
+    for cd in root.findall('CD'):
+        cd_dict = {}
+        for child in cd:
+            cd_dict[child.tag] = child.text
+        catalog.append(cd_dict)
+
+    # Eğer bir arama yapılmışsa, anahtar kelimeye göre filtrele
+    if request.method == "GET":
+        keyword = request.args.get("anahtarKelime", "").lower()
+        filtered_catalog = [
+            cd for cd in catalog if any(keyword in (v or "").lower() for v in cd.values())
+        ]
+        return render_template("katalog.html", data=filtered_catalog)    
+
+    return render_template("katalog.html", data=catalog)
+
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 80)))
+
+if __name__ == "__main__":
+    main()
